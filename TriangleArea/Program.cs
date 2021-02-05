0240f6200819423777710d20b664cc9ca3609595
Kevin Le <kevinle108@gmail.com>
2021-02-05 10:44:35 -0500

    area of three points

--- TriangleArea/Program.cs
+++ TriangleArea/Program.cs
@@ -26,10 +26,9 @@ namespace TriangleArea
             Console.Write("Enter y3: ");
             y3 = Convert.ToDouble(Console.ReadLine());
 
-            area = (x1*y2 + x2*y3 + x3*y1 - y1*x2 - y2*x3 - y3*x1) / 2;
+            area = (x1 * y2 + x2 * y3 + x3 * y1 - y1 * x2 - y2 * x3 - y3 * x1) / 2;
 
             Console.WriteLine($"Area is {area}");
-            
         }
     }
 }
